6a6102a2fcec41ff40815069d92b726f431a4d61
Amit Samanta <developer.amitsamanta@gmail.com>
2022-02-20 14:48:21 +0530

    Split date part into a separate component

--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,3 +1,4 @@
+import ExpenseDate from './ExpenseDate';
 import './ExpenseItem.css';
 
 const ExpenseItem = ({ 
@@ -10,17 +11,9 @@ const ExpenseItem = ({
     date: Date 
 }): JSX.Element => {
 
-    const month = date.toLocaleString('en-US', { month: 'long' });
-    const day = date.toLocaleString('en-US', { day: '2-digit' });
-    const year = date.getFullYear();
-
     return (
         <div className="expense-item">
-            <div>
-                <div>{month}</div>
-                <div>{year}</div>
-                <div>{day}</div>
-            </div>
+            <ExpenseDate date={date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
                 <div className="expense-item__price">₹{amount}</div>
