48901b3855ae3b9142bef9df4927a409ce021f29
Amit Samanta <developer.amitsamanta@gmail.com>
2022-02-20 13:22:35 +0530

    Pass data via props

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,51 @@ import React from 'react';
 import ExpenseItem from './components/ExpenseItem';
 
 function App() {
+  const expenses = [
+    { 
+      title: 'Car Insurance', 
+      amount: 294.67, date: 
+      new Date(2022, 2, 20)
+    },
+    { 
+      title: 'New Refrigirator', 
+      amount: 850.00, 
+      date: new Date(2021, 6, 25) 
+    },
+    { 
+      title: 'Juice', 
+      amount: 1.76, 
+      date: new Date(2022, 2, 20) 
+    },
+    { 
+      title: 'Curtains', 
+      amount: 66.78, 
+      date: new Date(2022, 2, 20) 
+    },
+  ];
+
   return (
     <div>
-      <ExpenseItem />
+      <ExpenseItem 
+        title={expenses[0].title}
+        amount={expenses[0].amount}
+        date={expenses[0].date}
+      />
+      <ExpenseItem 
+        title={expenses[1].title}
+        amount={expenses[1].amount}
+        date={expenses[1].date}
+      />
+      <ExpenseItem 
+        title={expenses[2].title}
+        amount={expenses[2].amount}
+        date={expenses[2].date}
+      />
+      <ExpenseItem 
+        title={expenses[3].title}
+        amount={expenses[3].amount}
+        date={expenses[3].date}
+      />
     </div>
   );
 }
--- src/components/ExpenseItem.tsx
+++ src/components/ExpenseItem.tsx
@@ -1,16 +1,21 @@
 import './ExpenseItem.css';
 
-const ExpenseItem = (): JSX.Element => {
-    const expenseDate = new Date(2022, 2, 20);
-    const expenseTitle = 'Car Insurance';
-    const expenseAmount = 294.97;
+const ExpenseItem = ({ 
+    title, 
+    amount, 
+    date 
+}: { 
+    title: string,
+    amount: number,
+    date: Date 
+}): JSX.Element => {
 
     return (
         <div className="expense-item">
-            <div>{expenseDate.toISOString()}</div>
+            <div>{date.toISOString()}</div>
             <div className="expense-item__description">
-                <h2>{expenseTitle}</h2>
-                <div className="expense-item__price">₹{expenseAmount}</div>
+                <h2>{title}</h2>
+                <div className="expense-item__price">₹{amount}</div>
             </div>
         </div>
     );
