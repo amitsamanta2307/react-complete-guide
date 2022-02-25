d48490acd7191985726cbb3cdba9c56795eee571
Amit Samanta <developer.amitsamanta@gmail.com>
2022-02-25 11:57:51 +0530

    Add state to the ExpenseItem component

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,28 +1,29 @@
+import { useState } from 'react';
+
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
 import './ExpenseItem.css';
 
-const ExpenseItem = ({ 
-    title, 
-    amount, 
-    date 
-}: { 
+const ExpenseItem = (props: { 
     title: string,
     amount: number,
     date: Date 
 }): JSX.Element => {
 
+    const [title, setTitle] = useState<string>(props.title);
+
     const handleClick = (): void => {
+        setTitle('Updated!');
         console.log('Cliked!!!');
     };
 
     return (
         <Card className="expense-item">
-            <ExpenseDate date={date} />
+            <ExpenseDate date={props.date} />
             <div className="expense-item__description">
                 <h2>{title}</h2>
-                <div className="expense-item__price">₹{amount}</div>
+                <div className="expense-item__price">₹{props.amount}</div>
             </div>
             <button onClick={handleClick}>Change Title</button>
         </Card>
