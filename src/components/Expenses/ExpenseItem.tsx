6e9d1a89fffdecef19ac3bc815c0b3396e8168fb
Amit Samanta <developer.amitsamanta@gmail.com>
2022-02-27 19:46:50 +0530

    Make ExpenseItem.tsx a stateless component

--- src/components/Expenses/ExpenseItem.tsx
+++ src/components/Expenses/ExpenseItem.tsx
@@ -1,5 +1,3 @@
-import { useState } from 'react';
-
 import Card from '../UI/Card';
 import ExpenseDate from './ExpenseDate';
 
@@ -10,22 +8,13 @@ const ExpenseItem = (props: {
     amount: number,
     date: Date 
 }): JSX.Element => {
-
-    const [title, setTitle] = useState<string>(props.title);
-
-    const handleClick = (): void => {
-        setTitle('Updated!');
-        console.log('Cliked!!!');
-    };
-
     return (
         <Card className="expense-item">
             <ExpenseDate date={props.date} />
             <div className="expense-item__description">
-                <h2>{title}</h2>
+                <h2>{props.title}</h2>
                 <div className="expense-item__price">₹{props.amount}</div>
             </div>
-            <button onClick={handleClick}>Change Title</button>
         </Card>
     );
 }
